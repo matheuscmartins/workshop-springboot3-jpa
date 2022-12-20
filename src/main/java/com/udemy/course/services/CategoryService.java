a3e0aeea34b576d42498da36b0dd2bfbc28a2050
Matheus Martins <matheus87martins@outlook.com>
2022-12-20 15:23:25 -0300

    Category

---
+++ src/main/java/com/udemy/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.udemy.course.services;
+
+import com.udemy.course.entities.Category;
+import com.udemy.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service //regista como serviço para ser injetado com o Autowired(Injeção de dependencia) nesse caso no UserResource
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.get();
+    }
+
+
+}
